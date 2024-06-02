0eba5eb231a30d79ac4fbf80eb9057d597ac18c1
SamsonGulma <sams0ngultek@gmail.com>
2024-06-02 16:49:45 +0300

    updates...

--- postingPage/assets/app.js
+++ postingPage/assets/app.js
@@ -1,1 +1,15 @@
-const 
+var num = 1;
+const addInputDescription = document.getElementsByClassName("simple-decription" + {num});
+
+var display = 0;
+
+function hideAndShow() {
+    if (display == 0) {
+        addInputDescription.style.display = 'block';
+        display = 1;
+    }
+    else {
+        display = 0;
+    }
+        
+}
