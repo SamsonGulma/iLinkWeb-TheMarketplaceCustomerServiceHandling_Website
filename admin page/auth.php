dc32c0bb4b2450864d41b4ec85b0b4c65d7efe8f
Yordi1362 <ybm.1362@gmail.com>
2024-06-26 14:01:38 -0700

    admin page

--- admin page/auth.php
+++ admin page/auth.php
@@ -5,7 +5,7 @@ session_start();
 // Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
     // User is not logged in, redirect to the login page
-    header("Location: /login.php"); // Redirect to your login page
+    header("Location:  /index.html"); // Redirect to your login page
     exit(); // Stop execution of further code
 }
 ?>
