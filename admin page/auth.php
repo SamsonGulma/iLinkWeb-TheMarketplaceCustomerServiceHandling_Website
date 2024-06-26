1674e1990e3a3e4c97bc8922c37e7fa7be2c6f15
Yordi1362 <ybm.1362@gmail.com>
2024-06-26 13:34:42 -0700

    amin page

---
+++ admin page/auth.php
@@ -0,0 +1,12 @@
+<?php
+// Start the session
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    // User is not logged in, redirect to the login page
+    header("Location: /login.php"); // Redirect to your login page
+    exit(); // Stop execution of further code
+}
+?>
+
