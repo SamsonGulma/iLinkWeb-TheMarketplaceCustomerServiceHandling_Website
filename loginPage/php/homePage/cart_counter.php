8eef292ce2e739053fb1a278cc466006199450a1
RobaByteNinja <166490682+RobaByteNinja@users.noreply.github.com>
2024-06-27 07:55:10 +0300

    Delete loginPage directory

--- loginPage/php/homePage/cart_counter.php
+++
@@ -1,5 +0,0 @@
-<?php
-$counter_cart = 0;
-$total_price = 0.0;
-$discount = 0.05;
-?>
--- loginPage/php/homePage/payment/pay.js
+++
@@ -1,8 +0,0 @@
-
-function PayByChap(price, user_id) {
-    // Adjust the URL to match your PHP script location
-    
-    alert("Payment functionality coming soon!");
-    // Create the alert message content
-    
-}
